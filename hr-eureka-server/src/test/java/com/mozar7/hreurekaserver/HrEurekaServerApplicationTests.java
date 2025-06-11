2db2e358833618c8c217037484f9f1f14fa27e76
AdsonMozart <mozar7.dev@gmail.com>
2025-06-11 01:33:45 -0300

    Project hr-eureka-server created

---
+++ hr-eureka-server/src/test/java/com/mozar7/hreurekaserver/HrEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mozar7.hreurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
